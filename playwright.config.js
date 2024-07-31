30a2a1f9b524fa1813189ef2bb1b63f51c6b16d2
Stolen <stolenmuppets@gmail.com>
2024-07-31 15:17:17 -0500

    lint

--- playwright.config.js
+++ playwright.config.js
@@ -1,4 +1,4 @@
-const { defineConfig, devices } = require('@playwright/test');
+const { defineConfig, devices } = require('@playwright/test')
 
 module.exports = defineConfig({
   testDir: './e2e-tests',
@@ -22,14 +22,14 @@ module.exports = defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-    webServer: {
-     command: 'npm run start',
-     url: 'http://127.0.0.1:5000',
-     reuseExistingServer: !process.env.CI,
+  webServer: {
+    command: 'npm run start',
+    url: 'http://127.0.0.1:5000',
+    reuseExistingServer: !process.env.CI,
   },
   use: {
     baseURL: 'http://localhost:5000',
     trace: 'on-first-retry'
   }
-});
+})
 
