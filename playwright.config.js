d4a772f6a1bc284e940c1759409f692689f7e4a1
Stolen <stolenmuppets@gmail.com>
2024-08-05 18:49:50 -0500

    asfd

--- playwright.config.js
+++ playwright.config.js
@@ -22,7 +22,7 @@ module.exports = defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run start',
+    command: 'npm run start-prod',
     url: 'http://127.0.0.1:5000',
     reuseExistingServer: !process.env.CI,
     timeout: 3 * 1000,
